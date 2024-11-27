9ff067ded0356f387715f2622ea09e169a45383b
Tiago Agostinho <46489201+tiagoagostinho1@users.noreply.github.com>
2024-11-27 16:37:58 +0000

    add tanstack router

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,23 @@ import { Footer } from "./components/Footer";
 // All packages except `@mantine/hooks` require styles imports
 import "@mantine/core/styles.css";
 
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, createTheme } from "@mantine/core";
+
+const theme = createTheme({
+  primaryColor: "bright-green",
+  colors: {
+    "bright-green": ["#495E57"],
+  },
+});
 
 function App() {
   return (
-    <MantineProvider>
+    <MantineProvider theme={theme}>
+      <meta
+        name="description"
+        content="Welcome to Little Lemon Restaurant – where fresh flavors and vibrant dishes meet exceptional dining. Enjoy a cozy atmosphere, locally-sourced ingredients, and a menu crafted to delight your taste buds. Perfect for family gatherings, date nights, and food lovers seeking unforgettable culinary experiences. Visit us today!"
+      />
+      <meta name="og:title" content="Little Lemon Restaurant" />
       <Header />
       <Main />
       <Footer />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,34 @@
+import {
+  IconBrandInstagram,
+  IconBrandTwitter,
+  IconBrandYoutube,
+} from "@tabler/icons-react";
+import { ActionIcon, Container, Group } from "@mantine/core";
+import classes from "./Footer.module.css";
+import { LittleLemonLogo } from "./LittleLemonLogo";
+
 export function Footer() {
-  return <footer>This is Footer</footer>;
+  return (
+    <div className={classes.footer}>
+      <Container className={classes.inner}>
+        <LittleLemonLogo />
+        <Group
+          gap={0}
+          className={classes.links}
+          justify="flex-end"
+          wrap="nowrap"
+        >
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandTwitter size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandYoutube size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandInstagram size={18} stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </div>
+  );
 }
